9d20a95a2d62154e8e47423f710ea9b257f6e815
aacebo <aacebowork@gmail.com>
2025-06-15 18:05:03 -0400

    Update node.go

--- src/ast/node.go
+++ src/ast/node.go
@@ -2,10 +2,6 @@ package ast
 
 import "github.com/thegogod/cmark/reflect"
 
-type Visitor interface {
-	Visit(node Node) (Visitor, error)
-}
-
 type Node interface {
 	Validate(scope *Scope) error
 	Evaluate(scope *Scope) (reflect.Value, error)
